3db8f863ac3bb590258fd45ea2125886f841a1f3
Muhammad Usman <sheikhusman545@gmail.com>
2025-11-04 23:58:05 +0500

    deleted

--- src/api/config/app-config.js
+++
@@ -1,9 +0,0 @@
-//const webServerURL = "http://192.168.43.8:412";
-//const webServerURL = "http://192.168.1.21:412";
-const webServerURL = "https://shopapi.alanaam.qa"; // "http://api.alanaam.tk" // "http://localhost:8080" 
-export const imageURL = (webServerURL + "/");
-
-export default {
-    webServerURL,
-    imageURL,
-}
